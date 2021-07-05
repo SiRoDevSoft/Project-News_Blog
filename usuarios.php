a8851c02533a7bb6a5157e4991565f2fe6a535c9
Siro <Silvio@Siro.net>
2021-07-05 18:35:38 -0300

    actualizando archivo de usuarios.php

--- usuarios.php
+++ usuarios.php
@@ -2,4 +2,8 @@
     include_once 'header.php';
     include_once 'principal.php';
     include_once 'credential.php';
-?>
+/*
+ Realizar modificaciones a este archivo para la administracion de usuarios*/
+
+
+
